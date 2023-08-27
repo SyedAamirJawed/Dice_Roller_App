2eec2b69b935897bad392470445651f398790dbb
SyedAamirJawed <syedaamirjawed@gmail.com>
2023-08-27 14:19:57 +0530

    Build Costom Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,28 +4,34 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 127, 67, 230),
-                Color.fromARGB(255, 69, 10, 172),
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              "Hello World",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-              ),
-            ),
-          ),
-        ),
+        body: MyBodyClass(),
       ),
     ),
   );
+}
+
+class MyBodyClass extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 127, 67, 230),
+            Color.fromARGB(255, 69, 10, 172),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello World..!",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
 }
